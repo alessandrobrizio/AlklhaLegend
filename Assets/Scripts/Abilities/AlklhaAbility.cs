c6305646992462be3d8ee2d0826709f2f656e236
Martina Massucco <martina.massucco96@gmail.com>
2020-11-06 00:19:26 +0100

    Alhkla animator and Alhkla abilities prototype

--- Assets/Scripts/Abilities/AlklhaAbility.cs
+++ Assets/Scripts/Abilities/AlklhaAbility.cs
@@ -2,7 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AlklhaAbility : Ability
+public abstract class AlklhaAbility : Ability
 {
-    
+    [SerializeField] protected float range = 2.0f;
+    [SerializeField] protected float cooldown = 3.0f;
+    [SerializeField] protected AnimatorOverrideController animatorOverrideCtrl = null;
+
+    public virtual void Cast(Alklha alklha)
+    {
+        Animator alklhaAnimator = alklha.GetComponent<Animator>();
+        if (alklhaAnimator == null)
+            return;
+
+        Debug.Log("Cast ");
+        alklhaAnimator.runtimeAnimatorController = animatorOverrideCtrl;
+        alklhaAnimator.SetTrigger("Attack");
+
+        alklha.AbilityTimer = cooldown + animatorOverrideCtrl["Attack"].length;
+    }
 }
--- Assets/Scripts/Alklha.cs
+++ Assets/Scripts/Alklha.cs
@@ -4,5 +4,45 @@ using UnityEngine;
 
 public class Alklha : MonoBehaviour
 {
-    
+    [SerializeField] AlklhaAbility[] abilities = null;
+    [SerializeField] float initialCooldown = 3.0f;
+
+    private int bossPhase = 0;
+    private float abilityTimer = 0.0f;
+    private Animator animator = null;
+
+    public float AbilityTimer{ set { abilityTimer = value; } }
+
+    private void Start()
+    {
+        abilityTimer = initialCooldown;
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        abilityTimer -= Time.deltaTime;
+        if(abilityTimer <= 0)
+        {
+            int i = Random.Range(0, abilities.Length);
+            abilities[i].Cast(this);
+        }
+        //chase player
+    }
+
+
+    private void OnMoonShot()
+    {
+        //TODO
+    }
+
+    private void RaiseBossPhaseEnd()
+    {
+        //TODO
+    }
+
+    private void RaiseGameOver()
+    {
+        //TODO
+    }
 }
