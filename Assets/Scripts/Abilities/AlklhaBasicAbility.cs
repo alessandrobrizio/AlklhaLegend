f2c55befa00dd7c7451a5f676113ea97de927bff
Martina Massucco <martina.massucco96@gmail.com>
2020-11-06 00:19:26 +0100

    Alhkla animator and Alhkla abilities prototype

--- Assets/Scripts/Abilities/AlklhaBasicAbility.cs
+++ Assets/Scripts/Abilities/AlklhaBasicAbility.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "AlklhaBasicAbility", menuName = "Ability/AlhklaAbility")]
 public class AlklhaBasicAbility : AlklhaAbility
 {
-    
+    public override void Cast(Alklha alklha)
+    {
+        base.Cast(alklha);
+        
+        //TODO: damage Player
+        // Area or front
+    }
 }
