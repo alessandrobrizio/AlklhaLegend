e6ca830dfd9142d621499a7913ea36ba1110730d
Martina Massucco <martina.massucco96@gmail.com>
2020-11-07 12:40:21 +0100

    Enemy prototype

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour
 {
-    
+
+    private Player player = null;
+    private NavMeshAgent agent = null;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        //TODO get player from GameManager
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //TODO: Damage player
+            Destroy(gameObject);
+        }
+    }
 }
