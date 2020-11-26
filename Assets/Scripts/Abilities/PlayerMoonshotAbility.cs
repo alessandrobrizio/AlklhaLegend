e5a674842d25eb7cd0bba9501507990d11343dc3
Martina Massucco <martina.massucco96@gmail.com>
2020-11-26 01:33:18 +0100

    Moonshot vfx script integration

--- Assets/Scripts/Abilities/PlayerMoonshotAbility.cs
+++ Assets/Scripts/Abilities/PlayerMoonshotAbility.cs
@@ -1,14 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 [CreateAssetMenu(fileName = "PlayerMoonshotAbility", menuName = "Ability/Player/Moonshot")]
 public class PlayerMoonshotAbility : PlayerAbility
 {
+    [SerializeField] private VisualEffectAsset moonshotVisualEffect = null;
+    [SerializeField] private float duration = 1.0f;
+    [SerializeField] private float moonRadius = 5.0f;
+
+
     public override void Cast(Player caster)
     {
         base.Cast(caster);
+        GameObject moonshot_spawnposition = GameObject.Find("Moonshot_spawnposition");
+        if(moonshot_spawnposition == null)
+        {
+            Debug.LogError("Player must have a child named moonshot_spawnposition with a visual effect component");
+        }
+        VisualEffect vfx = moonshot_spawnposition.GetComponent<VisualEffect>();
+        if (vfx == null)
+        {
+            Debug.LogError("Player must have a child named moonshot_spawnposition with a visual effect component");
+        }
+
+        vfx.visualEffectAsset = moonshotVisualEffect;
+
+        Vector3 offsetCaster = GameManager.Instance.Moon.transform.position - caster.transform.position;
+        offsetCaster.Normalize();
+        offsetCaster.y = 0.0f;
+        caster.transform.rotation = Quaternion.LookRotation(offsetCaster);
+
+        Vector3 offestMoonshot = GameManager.Instance.Moon.transform.position - moonshot_spawnposition.transform.position;
+        vfx.SetFloat("MoonshotMaxSize", offestMoonshot.magnitude - moonRadius);
+        offestMoonshot.Normalize();
+
+        moonshot_spawnposition.transform.rotation = Quaternion.LookRotation(offestMoonshot);
+
+        vfx.Play();
+        caster.StartCoroutine(StopMoonshotAfter(duration, vfx));
         Apply(caster, null);
+        
     }
 
     public override bool Apply(Player caster, Collider target)
@@ -20,5 +53,11 @@ public class PlayerMoonshotAbility : PlayerAbility
     private void RaiseMoonshot()
     {
         GameManager.Instance.moonshotEvent.Invoke();
+    }
+
+    private IEnumerator StopMoonshotAfter(float stopTime, VisualEffect vfx)
+    {
+        yield return new WaitForSeconds(stopTime);
+        vfx.Stop();
     }
 }
